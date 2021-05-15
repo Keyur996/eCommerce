1f062f92cecfde611c00ec54374f1331166be6e0
Keyur R. Machhi <Y05477@ad.csci.co.in>
2021-05-15 11:25:59 +0530

    fix mobile

--- src/app/pages/check-out/check-out.component.ts
+++ src/app/pages/check-out/check-out.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
-  styleUrls: ['./check-out.component.css']
+  styleUrls: ['./check-out.component.css'],
 })
 export class CheckOutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(value: any) {
+    console.log(value);
   }
-
 }
